eda1d1cf1e227e3cdce7a290c3d21ef938d9999e
erycktzalam <erycktzalam@gmail.com>
2023-04-18 22:05:51 -0600

    Creacion de clase nodo y clase matriz

---
+++ src/Matriz.java
@@ -0,0 +1,8 @@
+public class Matriz {
+
+    private Nodo root;
+
+    public Matriz() {
+        this.root = null;
+    }
+}
---
+++ src/Nodo.java
@@ -0,0 +1,56 @@
+public class Nodo {
+
+    private int valor;
+    private Nodo arriba;
+    private Nodo abajo;
+    private Nodo derecha;
+    private Nodo izquierda;
+
+    public Nodo (int valor){
+        this.valor=valor;
+        this.arriba = null;
+        this.abajo = null;
+        this.derecha = null;
+        this.izquierda = null;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public Nodo getArriba() {
+        return arriba;
+    }
+
+    public void setArriba(Nodo arriba) {
+        this.arriba = arriba;
+    }
+
+    public Nodo getAbajo() {
+        return abajo;
+    }
+
+    public void setAbajo(Nodo abajo) {
+        this.abajo = abajo;
+    }
+
+    public Nodo getDerecha() {
+        return derecha;
+    }
+
+    public void setDerecha(Nodo derecha) {
+        this.derecha = derecha;
+    }
+
+    public Nodo getIzquierda() {
+        return izquierda;
+    }
+
+    public void setIzquierda(Nodo izquierda) {
+        this.izquierda = izquierda;
+    }
+}
