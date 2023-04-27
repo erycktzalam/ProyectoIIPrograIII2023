597eb3dbdd64d53ecdcc0f9b58fb256e30e22b31
erycktzalam <erycktzalam@gmail.com>
2023-04-26 18:56:21 -0600

    Se agrega el metodo buscar el cual compara el dato ingresado con los datos de los vehiculos y el metodo eliminar el cual elimina el nodo que se requiera

--- src/Matriz.java
+++ src/Matriz.java
@@ -12,7 +12,7 @@ public class Matriz {
     private void inicializarMatriz() {
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz.length; j++) {
-                matriz[i][j] = new Nodo(new Vehiculo("ABC123","ROJO","2020","TOYOTA","ERICK TZALAM"));
+                matriz[i][j] = new Nodo(new Vehiculo("ABC123", "ROJO", "2020", "TOYOTA", "ERICK TZALAM"));
             }
         }
 
@@ -41,6 +41,40 @@ public class Matriz {
 
     public void setMatriz(Nodo[][] matriz) {
         this.matriz = matriz;
+    }
+
+    public void buscar(String dato) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (matriz[i][j].getVehiculo().getColor() == dato || matriz[i][j].getVehiculo().getLinea() == dato || matriz[i][j].getVehiculo().getModelo() == dato ||
+                        matriz[i][j].getVehiculo().getPlaca() == dato || matriz[i][j].getVehiculo().getPropietario() == dato) {
+                    matriz[i][j].getVehiculo().toString();
+                }
+            }
+        }
+    }
+
+    public void eliminar(int fila, int columna){
+        if (fila < 0 || fila >= matriz.length || columna < 0 || columna >= matriz[0].length) {
+            System.out.println("La posicion a eliminar es invalida");
+            return;
+        }
+
+        Nodo nodoEliminar = matriz[fila][columna];
+        if (nodoEliminar.getArriba() != null) {
+            nodoEliminar.getArriba().setAbajo(nodoEliminar.getAbajo());
+        }
+        if (nodoEliminar.getAbajo() != null) {
+            nodoEliminar.getAbajo().setArriba(nodoEliminar.getArriba());
+        }
+        if (nodoEliminar.getIzquierda() != null) {
+            nodoEliminar.getIzquierda().setDerecha(nodoEliminar.getDerecha());
+        }
+        if (nodoEliminar.getDerecha() != null) {
+            nodoEliminar.getDerecha().setIzquierda(nodoEliminar.getIzquierda());
+        }
+
+        matriz[fila][columna] = null;
     }
 
     public void imprimirMatriz() {
